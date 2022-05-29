d2a53495450a1651d69fb13f385d2eb3492f5d42
Sebastian <bacconsebastian@gmail.com>
2022-05-29 12:53:26 -0300

    correcciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+const nav = document.querySelector('nav')
 const corrector_nav = document.querySelector('.corrector_nav')
 
 window.addEventListener('scroll', () =>{
+    nav.classList.toggle('inactive', window.scrollY >= document.documentElement.clientHeight)
     corrector_nav.classList.toggle('active', window.scrollY >= document.documentElement.clientHeight)
 })
