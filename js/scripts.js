21e1c6038eb3adc4adeb808f7555715b99380562
Sebastian <bacconsebastian@gmail.com>
2022-05-29 12:44:57 -0300

    CV terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-const nav = document.querySelector('nav')
 const corrector_nav = document.querySelector('.corrector_nav')
 
 window.addEventListener('scroll', () =>{
-    nav.classList.toggle('active', window.scrollY > document.documentElement.clientHeight)
-    corrector_nav.classList.toggle('active', window.scrollY > document.documentElement.clientHeight)
+    corrector_nav.classList.toggle('active', window.scrollY >= document.documentElement.clientHeight)
 })
